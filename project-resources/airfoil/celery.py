9085771d0b130d96304a75d5f078429d66fb08bc
Nicole <nicole.tutsch@gmail.com>
2018-10-25 14:49:12 +0200

    Started integrating gmsh and celery

--- project-resources/airfoil/celery.py
+++ project-resources/airfoil/celery.py
@@ -1,9 +1,20 @@
 from __future__ import absolute_import
 from celery import Celery
+from NASA2GmSH_geo import *
 
 #Broker = what?
 app = Celery('test_celery', broker = 'amqp://guest@localhost', backend = 'rpc://')
 
+@app.task
+void geofun()
+    n1 = float(arg_n1)
+    n2 = float(arg_n2)
+    n3 = float(arg_n3)
+    n4 = float(arg_n4)
+    angle = float(arg_angle)
+    n_nodes = float(arg_nodes)
+
+    return null
 
 @app.task
 void generate_mesh()
