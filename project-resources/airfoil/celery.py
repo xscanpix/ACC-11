5654313f05bf1db469ad9d01221d96c883b7746f
Nicole <nicole.tutsch@gmail.com>
2018-10-25 14:54:30 +0200

    Changed it from java to python...

--- project-resources/airfoil/celery.py
+++ project-resources/airfoil/celery.py
@@ -6,7 +6,7 @@ from NASA2GmSH_geo import *
 app = Celery('test_celery', broker = 'amqp://guest@localhost', backend = 'rpc://')
 
 @app.task
-void geofun()
+def geofun(geofile, arg_n1, arg_n2, arg_n3, arg_n4, arg_angle, arg_nodes):
     n1 = float(arg_n1)
     n2 = float(arg_n2)
     n3 = float(arg_n3)
@@ -14,24 +14,29 @@ void geofun()
     angle = float(arg_angle)
     n_nodes = float(arg_nodes)
 
+    xs = np-linspace(0.0, 1.0, n_nodes)
+    x, y = naca4(n1, n2, n3, n4, xs)
+    xa, xy = rot(x,y, angle)
+    dat2gmsh(xa,ya)
+
     return null
 
 @app.task
-void generate_mesh()
+def generate_mesh():
 #Call gmsh
     return null
 
 @app.task
-void angle_aux()
+def angle_aux():
 #Gmsh help function
     return null
 
 @app.task
-void convert(var mesh)
+def convert(var mesh):
 #Convert one file form gmsh to XML with Dolfin-xml
     return null
 
 @app.task
-void calculate(var xml)
+def calculate(var xml):
 #Calculate the values
     return null
