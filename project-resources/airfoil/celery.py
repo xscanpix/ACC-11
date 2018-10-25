852ede389c8c55e490fd5b0c40c683451fe0d675
Nicole <nicole.tutsch@gmail.com>
2018-10-25 15:02:19 +0200

    Imported numpy and fixed an error

--- project-resources/airfoil/celery.py
+++ project-resources/airfoil/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from celery import Celery
 from nasa2gmsh_geo import *
+import numpy as np
 
 #Broker = what?
 app = Celery('test_celery', broker = 'amqp://guest@localhost', backend = 'rpc://')
@@ -14,7 +15,7 @@ def geofun(geofile, arg_n1, arg_n2, arg_n3, arg_n4, arg_angle, arg_nodes):
     angle = float(arg_angle)
     n_nodes = float(arg_nodes)
 
-    xs = np-linspace(0.0, 1.0, n_nodes)
+    xs = np.linspace(0.0, 1.0, n_nodes)
     x, y = naca4(n1, n2, n3, n4, xs)
     xa, xy = rot(x,y, angle)
     dat2gmsh(xa,ya)
