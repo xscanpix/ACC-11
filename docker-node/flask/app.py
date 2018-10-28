41de6d7196a69309a73d7aec28b653dc23ef36e7
martinregefalk <martin.regefalk@gmail.com>
2018-10-28 18:47:40 +0000

    form is working

--- docker-node/flask/app.py
+++ docker-node/flask/app.py
@@ -1,17 +1,23 @@
 #!flask/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import subprocess
 import sys
 
 app = Flask(__name__)
 
 
-@app.route('/app', methods=['GET'])
+@app.route('/app', methods=['GET', 'POST'])
 def start():    
+    if request.method == 'POST':
+        start_angle = request.values.get("start_angle")
+        end_angle  = request.values.get("end_angle")
+        n_angles = request.values.get("n_angles")
+
+
+        return render_template("hold.html", start_angle = start_angle, end_angle = end_angle,
+				 n_angles = n_angles )
+
     return render_template("home.html")
-
-
-
 
 
 
