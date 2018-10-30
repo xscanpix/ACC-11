d61fa213bd6e02e0ceea40105126c14089fbc648
Svante Nilsson <svante.n@live.se>
2018-10-30 13:16:50 +0100

    worked on solver, solves all xml files

--- docker-node/app/solver.py
+++ docker-node/app/solver.py
@@ -12,13 +12,13 @@ def main():
 
 def solve_all(srcdir):
     for filename in os.listdir(srcdir):
-        solve(filename)
+        solve(os.path.abspath(srcdir), filename)
 
 
-def solve(filename):
-    temp = filename.replace(".xml", "") + "_results"
-    os.system("{} 10 0.9 10 1 {}".format(SOLVERPATH, os.path.abspath(filename)))
-    os.rename(os.path.abspath("results"),  os.path.abspath("results").replace("results", temp))
+def solve(srcpath, filename):
+    resultsdirname = filename.replace(".xml", "") + "_results"
+    os.system("{} 10 0.9 10 1 {}".format(SOLVERPATH, "{}/{}".format(srcpath, filename)))
+    os.rename(os.path.abspath("results"),  os.path.abspath("results").replace("results", resultsdirname))
 
 
 if __name__ == "__main__":
