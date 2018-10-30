012f58d6d3554880b11bf0b22d56dbfc40e75051
Svante Nilsson <svante.n@live.se>
2018-10-30 14:50:27 +0100

    improving mesh generation

--- docker-node/app/mesh_generator.py
+++ docker-node/app/mesh_generator.py
@@ -11,8 +11,98 @@ def main():
     generate_meshes(angle_start, angle_stop, n_angles)
     convert_all_msh_xml()
 
+
+# Generate GEO file if it does not exist already
+def generate_geo(angle, dstdir, naca1=0, naca2=0, naca3=1, naca4=2, n_nodes=200, n_levels=0):
+    if not os.path.exists(dstdir):
+        os.mkdir(dstdir)
+
+    geofilename = "a{}n{}.geo".format(angle, n_nodes)
+
+    if os.path.exists("{}/{}".format(dstdir, geofilename)):
+        return os.path.abspath("{}/{}".format(dstdir, geofilename))
+
+    naca.generate(naca1, naca2, naca3, naca4, angle, n_nodes, "{}/{}".format(dstdir, geofilename))
+
+    return os.path.abspath("{}/{}".format(dstdir, geofilename))
+
+
+# Generate GEO file for every angle
+def generate_every_geo(angle_start, angle_stop, n_angles, dstdir="geo", naca1=0, naca2=0, naca3=1, naca4=2, n_nodes=200, n_levels=0):
+    anglediff = ((angle_stop-angle_start) / n_angles)
+
+    for i in  range(n_angles + 1):
+        angle = angle_start + anglediff*i
+        generate_geo(angle, dstdir)
+
+
+# Convert GEO file to MSH file if it does not exist already
+def convert_geo_to_msh(filepath, dstdir, gmshbin="/usr/bin/gmsh"):
+    if not os.path.exists(dstdir):
+        os.mkdir(dstdir)
+    
+    mshfilename = "r0" + ntpath.basename(filepath).replace(".geo", ".msh")
+
+    dstpath = os.path.abspath("{}/{}".format(dstdir, mshfilename))
+
+    if os.path.exists(dstpath):
+        return dstpath
+
+    os.system("{} -format auto -v 2 -2 -o {} {}".format(gmshbin, dstpath, filepath))
+
+    return dstpath
+
+
+# Convert every GEO file in a directory to a MSH file
+def convert_every_geo_to_msh(srcdir, dstdir, gmshbin="/usr/bin/gmsh"):
+    for filename in os.listdir(srcdir):
+        filepath = os.path.abspath("{}/{}".format(srcdir, filename))
+        convert_geo_to_msh(filepath, dstdir)
+
+
+# Convert MSH file to XML file if it does not exist already
+def convert_msh_to_xml(filepath, dstdir, dolfinconvertpath="/usr/bin/dolfin-convert"):
+    if not os.path.exists(dstdir):
+        os.mkdir(dstdir)
+
+    xmlfilename = ntpath.basename(filepath).replace(".msh", ".xml")
+    
+    dstpath = os.path.abspath("{}/{}".format(dstdir, xmlfilename))
+
+    if os.path.exists(dstpath):
+        return dstpath
+
+    os.system("{} {} {}".format(dolfinconvertpath, filepath, dstpath))
+
+    return dstpath
+
+
+# Convert every MSH file in directory to XML
+def convert_every_msh_to_xml(srcdir, dstdir, dolfinconvertpath="/usr/bin/dolfin-convert"):
+    for filename in os.listdir(srcdir):
+        filepath = os.path.abspath("{}/{}".format(srcdir, filename))
+        convert_msh_to_xml(filepath, dstdir)
+
+
+def generate_mesh(angle, gmshbin="/usr/bin/gmsh", geodir="geo", mshdir="msh", naca1=0, naca2=0, naca3=1, naca4=2, n_nodes=200, n_levels=0):
+    if not os.path.exists(geodir):
+        os.mkdir(geodir)
+    if not os.path.exists(mshdir):
+        os.mkdir(mshdir)
+
+    geofile = "{}/a{}n{}.geo".format(geodir, angle, n_nodes)
+    naca.generate(naca1, naca2, naca3, naca4, angle, n_nodes, geofile)
+
+    temp = geofile.replace(".geo","")
+    temp = "{}/r0{}.msh".format(mshdir, temp)
+    geo_name = "{}/{}".format(geodir, geofile)
+    os.system("{} -format auto -v 0 -2 -o {} {}".format(gmshbin, temp, geo_name))
+
+    return os.path.abspath(temp)
+
+
 def generate_meshes(angle_start, angle_stop, n_angles, gmshbin="/usr/bin/gmsh", geodir="geo", mshdir="msh", naca1=0, naca2=0, naca3=1, naca4=2, n_nodes=200, n_levels=0):
-    anglediff = ((angle_stop-angle_start)/n_angles)
+    anglediff = ((angle_stop-angle_start) / n_angles)
 
     if os.path.exists(geodir):
         shutil.rmtree(geodir)
@@ -48,10 +138,10 @@ def convert_all_msh_xml(srcdir="msh", dstdir="xml", dolfinconvertpath="/usr/bin/
         convert_one_msh_xml(srcfilepath, xmlabspath, dolfinconvertpath)
 
 
-def convert_one_msh_xml(filepath, dstdir, dolfinconvertpath):
-    absfilepath = os.path.abspath(filepath)
+def convert_one_msh_xml(srcfilepath, dstdir="xml", dolfinconvertpath="/usr/bin/dolfin-convert"):
+    absfilepath = os.path.abspath(srcfilepath)
     absdstdir = os.path.abspath(dstdir)
-    basename = ntpath.basename(filepath).replace(".msh", ".xml")
+    basename = ntpath.basename(srcfilepath).replace(".msh", ".xml")
     os.system("{} {} {}/{}".format(dolfinconvertpath, absfilepath, absdstdir, basename))
 
 
