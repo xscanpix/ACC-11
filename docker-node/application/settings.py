9642e602e6081b7882e4428549dda9162d47e907
Svante Nilsson <svante.n@live.se>
2018-11-01 19:58:15 +0100

    woerk

--- docker-node/application/settings.py
+++ docker-node/application/settings.py
@@ -1,5 +1,5 @@
 # ============================================================================
 # Celery config
 # ============================================================================
-CELERY_BROKER_URL = 'amqp://'
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
