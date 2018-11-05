48aa43ddaafa4e24b94610c92f8a03ee6bbfc344
Svante Nilsson <svante.n@live.se>
2018-11-05 14:00:30 +0100

    Work

--- project/application/app/webserver.py
+++ project/application/app/webserver.py
@@ -2,8 +2,8 @@ from flask import Flask
 from celery import Celery
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'amqp://rabbit:rabbit@10.0.2.15:5672'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:redis@10.0.2.15:6379/0'
+app.config['CELERY_BROKER_URL'] = 'amqp://rabbit:rabbit@10.10.10.15:5672'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:redis@10.10.10.15:6379/0'
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
 celery.conf.update(app.config)
